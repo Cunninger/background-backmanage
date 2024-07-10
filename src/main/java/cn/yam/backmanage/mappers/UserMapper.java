6e7e65d424a4dea7c86f22facce2b42c1f941000
Cunninger <1803912219@qq.com>
2024-07-10 14:30:34 +0800

    fix：权限编辑

--- src/main/java/cn/yam/backmanage/mappers/UserMapper.java
+++ src/main/java/cn/yam/backmanage/mappers/UserMapper.java
@@ -8,19 +8,4 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-//    User selectByUsername(String username);
-//
-//    void insert(User user);
-//
-//    @Select("SELECT * FROM user WHERE username LIKE CONCAT('%', #{search}, '%') LIMIT #{limit} OFFSET #{offset}")
-//    List<User> findUsers(@Param("search") String search, @Param("offset") int offset, @Param("limit") int limit);
-//
-//    @Select("SELECT COUNT(*) FROM user WHERE username LIKE CONCAT('%', #{search}, '%')")
-//    int countUsers(@Param("search") String search);
-//
-//    void deleteUser(int userId);
-//
-//    User selectByUserId(String s);
-//
-//    void update(User userTemp);
 }
